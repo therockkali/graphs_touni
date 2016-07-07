669efb3f923804c97db114b019b322217afa5da4
Gloria Budiman <wahyu.g@gmail.com>
2016-07-06 20:05:48 -0700

    Precompile glyphicons.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bo
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += ['*.js', '*.css']
+Rails.application.config.assets.precompile += ['*.js', '*.css', '*.woff', '*.eot', '*.svg', '*.ttf']
