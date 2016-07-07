0ed364c7edbe91e8ec269f48b92c8acfee92d093
Gloria Budiman <wahyu.g@gmail.com>
2016-07-07 01:27:25 -0700

    Highlight improvement.

--- app/assets/javascripts/viz.js
+++ app/assets/javascripts/viz.js
@@ -21,7 +21,7 @@ function viz(graph) {
        .gravity(1)
        .linkDistance(256)
        .linkStrength(1)
-       .friction(0.4);;
+       .friction(0.2);;
 
   /////////////////////////////////////
   // Code adapted from
@@ -40,7 +40,7 @@ function viz(graph) {
       .attr("orient", "auto")
     .append("path")
       .attr("d", "M0,-5L10,0L0,5 L10,0 L0, -5")
-      .style("stroke", "#4679BD")
+      .style("stroke", "#111")
       .style('stroke-width', '0.2em');
 
   /////////////////////////////////////
@@ -53,6 +53,8 @@ function viz(graph) {
       .insert('line')
       .style('marker-end', 'url(#end)')
       .attr('class', 'link-line')
+      .attr('source', function(d) { return d.source; })
+      .attr('target', function(d) { return d.target; })
       .attr('x1', function(d) { return nodes[d.source].x; })
       .attr('y1', function(d) { return nodes[d.source].y; })
       .attr('x2', function(d) { return nodes[d.target].x; })
@@ -62,6 +64,7 @@ function viz(graph) {
       .insert('svg:text')
       .attr('class', 'link-label')
       .attr('source', function(d) { return d.source; })
+      .attr('target', function(d) { return d.target; })
       .text(function(d) {
         return d.weight;
       })
@@ -71,8 +74,9 @@ function viz(graph) {
 
   var node_enter = node.enter().append('g')
       .attr('class', 'node')
+      .attr('node-id', function(d) { return d.id; })
       .call(force.drag)
-      .on('dblclick', connectedNodes);
+      .on('click', connectedNodes);
 
   node_enter.insert('svg:circle')
     .attr('r', '1em');  
@@ -144,18 +148,23 @@ function viz(graph) {
       return linkedByIndex[a.index + "," + b.index];
   }
   function connectedNodes() {
+    if (d3.event.defaultPrevented) {
+      return;
+    }
+
     var selected = d3.select(this).node().__data__.id;
     if (toggle == 0 || toggled_index != selected) {
       //Reduce the opacity of all but the neighbouring nodes
       d = d3.select(this).node().__data__;
       node.style("opacity", function (o) {
-        //return neighboring(d, o) | neighboring(o, d) ? 1 : 0.1;
-        return neighboring(d, o) ? 1 : 0.1;
+        return neighboring(d, o) | neighboring(o, d) ? 1 : 0.1;
+        //return neighboring(d, o) ? 1 : 0.1;
       });
       link.style("opacity", function (o) {
-        //return d.index==o.source.index | d.index==o.target.index ? 1 : 0.1;
-        return d.index == o.source.index ? 1 : 0.1;
+        return d.index==o.source.index | d.index==o.target.index ? 1 : 0.1;
+        //return d.index == o.source.index ? 1 : 0.1;
       });
+
       //Reduce the op
       toggle = 1;
 
@@ -165,6 +174,36 @@ function viz(graph) {
 
       $('.link-label').attr('opacity', 0.1);
       $('.link-label[source=' + hid + ']').attr('opacity', 1);
+      $('.link-label[target=' + hid + ']').attr('opacity', 1);
+      $('.node')
+        .css('fill', '#fff')
+        .css('stroke', '#1E72AD')
+        .find('text')
+          .css('fill', '#1E72AD')
+          .css('stroke', '#1E72AD');
+
+      node.style('stroke', function (o) {
+        //return neighboring(o, d) ? '#d33' : '1E72AD';
+        if (neighboring(d, o) && neighboring(o, d)) {
+          return '#3a3';
+        } else if (neighboring(d, o)) {
+          return '#1E72AD';
+        } else if (neighboring(o, d)) {
+          return '#d33';
+        }
+      });
+
+      $('.node').find('text')
+        .css('fill', '#333')
+        .css('stroke', '#333')
+      $('.node[node-id=' + hid + ']')
+        .css('stroke', '#111')
+        .css('fill', '#111')
+        .find('text')
+          .css('stroke', '#fff')
+          .css('fill', '#fff');
+
+      $('#highlight-info').css('visibility', 'visible');
     } else {
       //Put them back to opacity=1
 
@@ -172,6 +211,13 @@ function viz(graph) {
       link.style("opacity", 1);
       toggle = 0;
       $('.link-label').attr('opacity', 1);
+      $('.node')
+        .css('fill', '#fff')
+        .css('stroke', '#1E72AD')
+        .find('text')
+          .css('fill', '#1E72AD')
+          .css('stroke', '#1E72AD');
+      $('#highlight-info').css('visibility', 'hidden');
     }
   }
 }
